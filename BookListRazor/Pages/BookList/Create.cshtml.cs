5538f4182c5a3b47109c5b3163e051d73c92887e
shuan86 <danny90422@gmail.com>
2020-07-21 21:33:22 +0800

    first commit

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BookListRazor.Model;
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public Book Book { set; get; }
+        public async Task OnGet()
+        {
+           // Book = await _db.Book.ToListAsync();
+            
+        }
+    }
+}
