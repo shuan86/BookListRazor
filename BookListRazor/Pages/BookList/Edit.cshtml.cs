d266c75e0e42224825b3be36c51d1f07b8a09228
shuan86 <danny90422@gmail.com>
2020-07-24 18:14:53 +0800

    finished edit page

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -4,38 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using BookListRazor.Model;
 namespace BookListRazor.Pages.BookList
 {
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
-
-        private readonly ApplicationDbContext _db;
-        public CreateModel(ApplicationDbContext db)
+        private ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
-
         }
         public Book Book { set; get; }
-        public async Task OnGet()
+        public async Task OnGet(int id)
         {
-           // Book = await _db.Book.ToListAsync();
-            
+            Book = await  _db.Book.FindAsync(id);
         }
         public async Task<IActionResult> OnPost(Book book)
         {
             if (ModelState.IsValid)
             {
-                await _db.AddAsync(book);
+                var dbBook=await _db.Book.FindAsync(book.Id);
+                dbBook.Author = book.Author;
+                dbBook.ISBN = book.ISBN;
+                dbBook.Name = book.Name;
                 await _db.SaveChangesAsync();
                 return RedirectToPage("Index");
             }
-            else
-            {
-                return Page();
-            }
-            return Page();
+            return RedirectToPage();
         }
     }
 }
